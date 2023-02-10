124d486822e09c8c1ca9987f2d8db1a2ffe126be
Jose Antonio <antoniojaraujo0@gmail.com>
2023-02-09 22:40:54 -0300

    making our first POST request

--- server.js
+++ server.js
@@ -19,16 +19,34 @@ const friends = [
 	},
 ];
 
+// NOTE first middleware to run
 app.use((req, res, next) => {
-    // register the time the request was made
+	// register the time the request was made
 	const start = Date.now();
-    //passes foward
+	//passes foward
 	next();
-    //return from endPoint, register the time from request to right before response...
+	//return from endPoint, register the time from request to right before response...
 	const delta = Date.now() - start;
-    //log method, url and time passed
+	//log method, url and time passed
 	console.log(`${req.method} ${req.url} ${delta}`);
 });
+
+app.use(express.json());
+
+app.post("/friends", (req, res) => {
+	if (!req.body.name) {
+		return res.status(400).json({ error: "name is required" });
+	}
+
+	const newFriend = {
+		name: req.body.name,
+		id: friends.length,
+	};
+	friends.push(newFriend);
+
+	res.json(newFriend);
+});
+
 
 app.get("/friends", (_, res) => {
 	res.json(friends);
